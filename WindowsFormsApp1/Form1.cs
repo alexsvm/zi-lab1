113380cfa0ac4d36933fdf259197f4f54af545b1
MamakovAV <alexs.vm@gmail.com>
2018-11-09 16:07:20 +0300

    first

--- WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/Form1.cs
@@ -16,5 +16,18 @@ namespace WindowsFormsApp1
         {
             InitializeComponent();
         }
+
+        public int GetNOD(int val1, int val2)
+        {
+            if (val2 == 0)
+                return val1;
+            else
+                return GetNOD(val2, val1 % val2);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox2.Text = GetNOD(Decimal.ToInt32(edA.Value), Decimal.ToInt32(edK.Value)).ToString();
+        }
     }
 }

c825db36bcb1b575ea0c6986110a73199b239e55
alexs m <alexs.vm@gmail.com>
2018-11-11 22:16:11 +0300

    ASSC method done?

--- WindowsFormsApp1/Form1.Designer.cs
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.textBox2 = new System.Windows.Forms.TextBox();
             this.btnEncrypt = new System.Windows.Forms.Button();
@@ -37,8 +38,16 @@
             this.label2 = new System.Windows.Forms.Label();
             this.edK = new System.Windows.Forms.NumericUpDown();
             this.button1 = new System.Windows.Forms.Button();
+            this.edAlphabet = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lblM = new System.Windows.Forms.Label();
+            this.btnInit = new System.Windows.Forms.Button();
+            this.textLog = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.Hint = new System.Windows.Forms.ToolTip(this.components);
             ((System.ComponentModel.ISupportInitialize)(this.edA)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.edK)).BeginInit();
+            this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
             // textBox1
@@ -50,32 +59,34 @@
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(21, 187);
+            this.textBox2.Location = new System.Drawing.Point(12, 90);
             this.textBox2.Name = "textBox2";
             this.textBox2.Size = new System.Drawing.Size(427, 20);
             this.textBox2.TabIndex = 1;
             // 
             // btnEncrypt
             // 
-            this.btnEncrypt.Location = new System.Drawing.Point(21, 137);
+            this.btnEncrypt.Location = new System.Drawing.Point(12, 61);
             this.btnEncrypt.Name = "btnEncrypt";
             this.btnEncrypt.Size = new System.Drawing.Size(113, 23);
             this.btnEncrypt.TabIndex = 2;
             this.btnEncrypt.Text = "Encrypt";
             this.btnEncrypt.UseVisualStyleBackColor = true;
+            this.btnEncrypt.Click += new System.EventHandler(this.btnEncrypt_Click);
             // 
             // btnDecrypt
             // 
-            this.btnDecrypt.Location = new System.Drawing.Point(140, 137);
+            this.btnDecrypt.Location = new System.Drawing.Point(131, 61);
             this.btnDecrypt.Name = "btnDecrypt";
             this.btnDecrypt.Size = new System.Drawing.Size(113, 23);
             this.btnDecrypt.TabIndex = 3;
             this.btnDecrypt.Text = "Decrypt";
             this.btnDecrypt.UseVisualStyleBackColor = true;
+            this.btnDecrypt.Click += new System.EventHandler(this.btnDecrypt_Click);
             // 
             // edA
             // 
-            this.edA.Location = new System.Drawing.Point(52, 87);
+            this.edA.Location = new System.Drawing.Point(41, 326);
             this.edA.Minimum = new decimal(new int[] {
             1,
             0,
@@ -93,24 +104,26 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(32, 89);
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(18, 328);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(17, 13);
+            this.label1.Size = new System.Drawing.Size(19, 13);
             this.label1.TabIndex = 6;
             this.label1.Text = "A:";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(155, 89);
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(98, 328);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(17, 13);
+            this.label2.Size = new System.Drawing.Size(19, 13);
             this.label2.TabIndex = 8;
             this.label2.Text = "K:";
             // 
             // edK
             // 
-            this.edK.Location = new System.Drawing.Point(175, 87);
+            this.edK.Location = new System.Drawing.Point(118, 326);
             this.edK.Minimum = new decimal(new int[] {
             1,
             0,
@@ -127,7 +140,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(289, 137);
+            this.button1.Location = new System.Drawing.Point(589, 167);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(102, 23);
             this.button1.TabIndex = 9;
@@ -135,11 +148,75 @@
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
+            // edAlphabet
+            // 
+            this.edAlphabet.Location = new System.Drawing.Point(21, 273);
+            this.edAlphabet.Name = "edAlphabet";
+            this.edAlphabet.Size = new System.Drawing.Size(495, 20);
+            this.edAlphabet.TabIndex = 10;
+            this.edAlphabet.Text = "АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ ,.0123456789";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(18, 257);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(62, 13);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "Алфавит:";
+            // 
+            // lblM
+            // 
+            this.lblM.AutoSize = true;
+            this.lblM.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lblM.Location = new System.Drawing.Point(18, 296);
+            this.lblM.Name = "lblM";
+            this.lblM.Size = new System.Drawing.Size(46, 13);
+            this.lblM.TabIndex = 12;
+            this.lblM.Text = "M = 46";
+            // 
+            // btnInit
+            // 
+            this.btnInit.Location = new System.Drawing.Point(21, 390);
+            this.btnInit.Name = "btnInit";
+            this.btnInit.Size = new System.Drawing.Size(75, 23);
+            this.btnInit.TabIndex = 13;
+            this.btnInit.Text = "Init";
+            this.btnInit.UseVisualStyleBackColor = true;
+            this.btnInit.Click += new System.EventHandler(this.btnInit_Click);
+            // 
+            // textLog
+            // 
+            this.textLog.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.textLog.Font = new System.Drawing.Font("Lucida Console", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textLog.Location = new System.Drawing.Point(3, 16);
+            this.textLog.Multiline = true;
+            this.textLog.Name = "textLog";
+            this.textLog.Size = new System.Drawing.Size(995, 198);
+            this.textLog.TabIndex = 14;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.textLog);
+            this.groupBox1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.groupBox1.Location = new System.Drawing.Point(0, 419);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(1001, 217);
+            this.groupBox1.TabIndex = 15;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Лог";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1001, 636);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.btnInit);
+            this.Controls.Add(this.lblM);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.edAlphabet);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.edK);
@@ -153,6 +230,8 @@
             this.Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)(this.edA)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.edK)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -169,6 +248,13 @@
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.NumericUpDown edK;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox edAlphabet;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lblM;
+        private System.Windows.Forms.Button btnInit;
+        private System.Windows.Forms.TextBox textLog;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.ToolTip Hint;
     }
 }
 
--- WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/Form1.cs
@@ -8,15 +8,32 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+// З16-ИВТ, Мамаков А.В.
+// Вариант №9
+// 46 символов (А…Я, пробел, «,», «.», цифры 0…9 ), методы 3 и 9
+// M=
+//
+
 namespace WindowsFormsApp1
 {
     public partial class Form1 : Form
     {
+        // Аффинная система подстановок Цезаря (3):
+        int A, K, M; // А, К - ключи, М - длина словаря
+        int[] SubstTable; // Таблица  кодов для аффинных подстановок I(J)
+        string Alphabet; // Алфавит
+        Dictionary<char, char> EnCodingTable; // Словарь символов для прямой подстановки - кодирования (Исх. символ, Код. символ)
+        Dictionary<char, char> DeCodingTable; // Словарь символов для обратной подстановки - декодировния (Код. символ, Исх. символ)
+
+        // Метод шифрующих таблиц с одиночной перестановкой по ключу (9)
+
+
         public Form1()
         {
             InitializeComponent();
         }
 
+        // Функция вычисляющяя НОД двух чисел
         public int GetNOD(int val1, int val2)
         {
             if (val2 == 0)
@@ -25,9 +42,112 @@ namespace WindowsFormsApp1
                 return GetNOD(val2, val1 % val2);
         }
 
+        private bool ValidateASSCKeys() // Проверка:  0 ≤ (A, J)≤ (M-1),  0 ≤ K ≤ (M-1),  НОД (A, M)=1
+        { //A, K, M
+            Hint.Hide(edA);
+            Hint.Hide(edK);
+            if (A >= M)
+            {
+                Hint.Show("A должно быть меньше длины алфавита!", edA, 0, 0);
+                //PointToClient(new Point(edA.Left, edA.Top))
+                return false;
+            }
+            if (K >= M)
+            {
+                Hint.Show("K должно быть меньше длины алфавита!", edK, 0, 0);
+                return false;
+            }
+            if (GetNOD(A, M) != 1)
+            {
+                Hint.Show("A и М должны иметь наименьший общий делитель = 1!", edA, 0, 0);
+                return false;
+            }
+            return true;
+        }
+
+        // Инициализируем A, K, M
+        public void Init()
+        {
+            A = Decimal.ToInt32(edA.Value);
+            K = Decimal.ToInt32(edK.Value);
+            M = Decimal.ToInt32(edAlphabet.Text.Length);
+            lblM.Text = "M = " + M.ToString();
+            if (!ValidateASSCKeys()) // Проверка:  0 ≤ (A, J)≤ (M-1),  0 ≤ K ≤ (M-1),  НОД (A, M)=1
+                return;
+            // Алфавит:
+            Alphabet = edAlphabet.Text;
+            // Инициализация таблицу кодов для аффинных подстановок при A, K, M 
+            SubstTable = new int[M];
+            for (int j = 0; j < M; j++) // I = (А∙J+K) mod M
+                SubstTable[j] = (A * j + K) % M;
+            // Отобразим таблицу в логе:
+            textLog.Text += "таблица кодов для аффинных подстановок при A, K, M" + Environment.NewLine;
+            for (int j = 0; j < M; j++)
+                textLog.Text += string.Format("{0,2}", j) + "|";
+            textLog.Text += Environment.NewLine;
+            for (int j = 0; j < M; j++)
+                textLog.Text += string.Format("{0,2}", SubstTable[j]) + "|";
+            //
+            // Инициализируем словарь символов для афинных подстановок
+            EnCodingTable = new Dictionary<char, char>();
+            DeCodingTable = new Dictionary<char, char>();
+            for (int j = 0; j < M; j++)
+            {
+                EnCodingTable.Add(Alphabet[j], Alphabet[SubstTable[j]]);
+                DeCodingTable.Add(Alphabet[SubstTable[j]], Alphabet[j]);
+            }
+            // Log
+            textLog.Text += Environment.NewLine + "словарь символов для афинных подстановок" + Environment.NewLine;
+            foreach (var kvp in EnCodingTable)
+                textLog.Text += string.Format("('{0}'-'{1}') ", kvp.Key, kvp.Value);
+        }
+
+        private string EncodeASSC(string str)
+        {
+            string res = "";
+            foreach (char c in str)
+            {
+                // Проверить, есть симвоол в алфавите!
+                if (!EnCodingTable.ContainsKey(c))
+                    return "";
+                // Перекодировать символ и добавить в результат!
+                res += EnCodingTable[c];
+            }
+            return res;
+        }
+
+        private string DecodeASSC(string str)
+        {
+            string res = "";
+            foreach (char c in str)
+            {
+                // Проверить, есть симвоол в алфавите!
+                if (!DeCodingTable.ContainsKey(c))
+                    return "";
+                // Перекодировать символ и добавить в результат!
+                 res += DeCodingTable[c];
+            }
+            return res;
+        }
+
+        private void btnEncrypt_Click(object sender, EventArgs e)
+        {
+            textBox2.Text = EncodeASSC(textBox1.Text);
+        }
+
+        private void btnDecrypt_Click(object sender, EventArgs e)
+        {
+            textBox2.Text = DecodeASSC(textBox1.Text);
+        }
+
         private void button1_Click(object sender, EventArgs e)
         {
             textBox2.Text = GetNOD(Decimal.ToInt32(edA.Value), Decimal.ToInt32(edK.Value)).ToString();
+        }
+
+        private void btnInit_Click(object sender, EventArgs e)
+        {
+            Init();
         }
     }
 }
